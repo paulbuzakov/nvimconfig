53f6d69a769d181b87f8a8835f3a01827e3ae228
Paul Buzakov <paulbuzakov@gmail.com>
2024-04-30 00:42:38 +0500

    feat(#2): keymaps for tab and select all text
    feat #2

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymaps = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Tabs
+keymaps.set("n", "te", ":tabedit")
+keymaps.set("n", "<tab>", ":tabnext<Return>", opts)
+keymaps.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- Select all
+keymaps.set("n", "<C-a>", "ggVG")
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transpatent = true,
+      }
+    end,
+  },
+}
