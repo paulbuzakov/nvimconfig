c7027c740eb2d0491cfadb48cf73dfad77ef10f5
Paul Buzakov <paulbuzakov@Pauls-MacBook-Pro.local>
2024-07-06 16:37:58 +0500

    feat: #13 Auto format and save when exit insert mode
    refs:
    https://github.com/paulbuzakov/nvimconfig/issues/13

--- lua/dev-as-samurai/6_autocmd.lua
+++ lua/dev-as-samurai/6_autocmd.lua
@@ -1,7 +1,13 @@
 local M = {}
 
 function M.setup()
-
+  vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+    pattern = "*.*",
+    callback = function()
+      vim.cmd("lua vim.lsp.buf.format()")
+      vim.cmd("update")
+    end,
+  })
 end
 
 return M
