7ae54ebc1463f5fd2311f750ad9cfab6dd9255f6
Paul Buzakov <paulbuzakov@gmail.com>
2025-06-07 23:07:36 +0500

    Some changed the catppuccin colore schema.
    Signed-off-by: Paul Buzakov <paulbuzakov@gmail.com>

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "saghen/blink.cmp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        version = "1.*",
+        opts = {
+            completion = {
+                menu = { border = "single" },
+                documentation = { window = { border = "single" } },
+            },
+            signature = { window = { border = "single" } },
+        },
+    },
+}
---
+++ lua/plugins/nui.lua
@@ -0,0 +1,5 @@
+return {
+    "MunifTanjim/nui.nvim",
+    commit = "8d3bce9764e627b62b07424e0df77f680d47ffdb",
+}
+
